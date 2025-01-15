b3c2923b18a039e3afe5ec4a3667ff0a3bea443a
rodrigo <javierluis15975@hotmail.com>
2025-01-15 03:55:39 -0500

    update pathServices.ts

--- src/pathServices/pathServices.ts
+++ src/pathServices/pathServices.ts
@@ -1,16 +1,16 @@
 /* eslint-disable no-unused-vars */
-export const enum PathServices {
-  URL = 'http://localhost:4000',
-  URL_FILES = 'http://localhost:8080',
-  ROLE = '/role',
-  USER = '/user',
-  PROFILE = '/profile/token',
-  AUTH = '/auth/login',
-  VERIFY_TOKEN = '/auth/verify-token',
-  LOGOUT = '/auth/logout',
-  CATEGORIES = '/category',
-  CATEGORIESDISCOUNT = '/category/discount',
-  PRODUCTS = '/products',
-  FILES = '/files',
-  COUPON = '/coupon',
-}
+export const PathServices = {
+  URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
+  URL_FILES: process.env.NEXT_PUBLIC_API_FILES || 'http://localhost:8080',
+  ROLE: '/role',
+  USER: '/user',
+  PROFILE: '/profile/token',
+  AUTH: '/auth/login',
+  VERIFY_TOKEN: '/auth/verify-token',
+  LOGOUT: '/auth/logout',
+  CATEGORIES: '/category',
+  CATEGORIESDISCOUNT: '/category/discount',
+  PRODUCTS: '/products',
+  FILES: '/files',
+  COUPON: '/coupon',
+} as const
